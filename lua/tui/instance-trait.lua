51e8a579b4308e44cd513baaf01b613b88e97e48
Sam Sze <mingsum.sam@gmail.com>
2024-07-11 21:40:32 +0800

    update

--- lua/tui/instance-trait.lua
+++ lua/tui/instance-trait.lua
@@ -10,7 +10,7 @@ Instance.__is_class = true
 setmetatable(Instance, { __index = Controller })
 
 -- Configure controller UI hooks
-function Instance:_setup_controller_ui_hooks()
+function Instance:setup_controller_ui_hooks()
   self:set_ui_hooks({
     show = function() self.layout:show() end,
     hide = function() self.layout:hide() end,
