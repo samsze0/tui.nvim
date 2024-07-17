2280989b4b667de184790bd83be0471e9fa9a836
Sam Sze <mingsum.sam@gmail.com>
2024-07-18 00:54:24 +0800

    rename trait to mixin

--- lua/tui/instance-mixin.lua
+++ lua/tui/instance-mixin.lua
@@ -2,14 +2,14 @@ local TUIController = require("tui.controller")
 local oop_utils = require("utils.oop")
 local tbl_utils = require("utils.table")
 
--- A trait. Should not be inherited directly.
+-- A mixin. Should not be inherited directly.
 --
----@class TUIInstanceTrait : TUIController
+---@class TUIInstanceMixin : TUIController
 ---@field layout TUILayout
-local TUIInstanceTrait = oop_utils.new_class(TUIController)
+local TUIInstanceMixin = oop_utils.new_class(TUIController)
 
 -- Configure controller UI hooks
-function TUIInstanceTrait:setup_controller_ui_hooks()
+function TUIInstanceMixin:setup_controller_ui_hooks()
   self:set_ui_hooks({
     show = function() self.layout:show() end,
     hide = function() self.layout:hide() end,
@@ -21,7 +21,7 @@ end
 -- TODO: move to private config
 ---@param target_popup TUISidePopup
 ---@param opts? { force?: boolean }
-function TUIInstanceTrait:setup_scroll_keymaps(target_popup, opts)
+function TUIInstanceMixin:setup_scroll_keymaps(target_popup, opts)
   opts = opts or {}
 
   self.layout.main_popup:map_remote(
@@ -52,7 +52,7 @@ end
 
 -- TODO: move to private config
 ---@param opts? { force?: boolean }
-function TUIInstanceTrait:setup_close_keymaps(opts)
+function TUIInstanceMixin:setup_close_keymaps(opts)
   opts = opts or {}
 
   for _, popup in ipairs(tbl_utils.values(self.layout.side_popups)) do
@@ -63,4 +63,4 @@ function TUIInstanceTrait:setup_close_keymaps(opts)
   end
 end
 
-return TUIInstanceTrait
+return TUIInstanceMixin
